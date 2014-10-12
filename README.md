e2f40117d95fecc05a340ca0c1276207a463e401
Gary Rowe <g.rowe@froot.co.uk>
2014-10-12 16:05:07 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-hid4java
-========
+Status: [![Build Status](https://travis-ci.org/gary-rowe/hid4java.png?branch=master)](https://travis-ci.org/gary-rowe/hid4java)
 
-A Java Native Access (JNA) wrapper for USB Human Interface Devices (HID) that is cross-platform and works out of the box
+### Project status
+
+Early-Beta: Expect bugs and minimal API changes. Not suitable for production, but developers should start integrating.
+
+### hid4java 
+
+The hid4java project supports USB HID devices through a common API which is provided here under the MIT license.
+
+### Technologies
+
+* [hidapi](https://github.com/signal11/hidapi) - Native USB HID library for multiple platforms
+* [JNA](https://github.com/twall/jna) - to remove the need for Java Native Interface (JNI) and greatly simplify the project
+* Java 7+ - to remove dependencies on JVMs that have reached end of life
+
+### Code example
+
+```java
+
+```
+ 
+### Getting started
+
+The project uses the standard Maven build process and can be used without having external hardware attached. Just do the usual
+
+```
+$ cd <project directory>
+$ mvn clean install
+```
+
+and you're good to go. Your next step is to explore the examples.
+
+### Frequently asked questions (FAQ)
+
+#### What platforms do you support ?
+
+If you have a native version of `hidapi` for your platform then you'll be able to support it. 
+
+Pre-compiled versions for Windows (32/64), OS X (10.5+) and Linux (32/64) are provided.
+
+#### Why not just use usb4java ?
+
+The usb4java project, while superb, does not support HID devices on OS X and there are no plans to introduce HID support anytime soon.
+ 
+#### Can I just copy this code into my project ?
+
+Yes. Perhaps you'd prefer to use 
+
+```
+git submodule add https://github.com/gary-rowe/hid4java hid4java 
+```
+so that you can keep up to date with changes whilst still fixing the version. 
+
+
+### Troubleshooting
+
+The following are known issues and their solutions or workarounds.
+
+#### I get a `SIGSEGV (0xb)` when starting up
+
+You have probably got the `getFieldOrder` list wrong. Use the field list from Class.getFields() to get a suitable order.
+
+### Closing notes
+
+All trademarks and copyrights are acknowledged.
+
+Many thanks to victorix who provided the basis for this library. Please see the inspiration <a href="http://developer.mbed.org/cookbook/USBHID-bindings-">on the mbed.org site.</a></p>
---
+++ src/main/test/org/hid4java/UsbHidTrezorV1Example.java
@@ -0,0 +1,154 @@
+package org.hid4java;
+
+import org.hid4java.event.HidServicesEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+/**
+ * <p>Demonstrate the USB HID interface using a production Bitcoin Trezor</p>
+ *
+ * @since 0.0.1
+ *  
+ */
+public class UsbHidTrezorV1Example implements HidServicesListener {
+
+  static final int PACKET_LENGTH = 64;
+  private HidServices hidServices;
+
+  public static void main(String[] args) throws HidException {
+
+    UsbHidTrezorV1Example example = new UsbHidTrezorV1Example();
+    example.executeExample();
+
+  }
+
+  public void executeExample() throws HidException {
+
+    System.out.println("Loading hidapi...");
+
+    // Get HID services
+    hidServices = HidManager.getHidServices();
+    hidServices.addUsbServicesListener(this);
+
+    // Provide a list of attached devices
+    for (HidDeviceInfo hidDeviceInfo : hidServices.getAttachedHidDevices()) {
+      System.out.println(hidDeviceInfo);
+    }
+
+    // Open the Trezor device by Vendor ID and Product ID with wildcard serial number
+    HidDevice trezor = hidServices.getHidDevice(0x534c, 0x01, null);
+    if (trezor != null) {
+      // Device is already attached so send message
+      sendInitialise(trezor);
+    }
+
+    // Stop the main thread to demonstrate attach and detach events
+    sleepUninterruptibly(1, TimeUnit.HOURS);
+
+  }
+
+  @Override
+  public void hidDeviceAttached(HidServicesEvent event) {
+
+    System.out.println("Device attached: " + event);
+
+    if (event.getHidDeviceInfo().getVendorId() == 0x534c &&
+      event.getHidDeviceInfo().getProductId() == 0x01) {
+
+      // Open the Trezor device by Vendor ID and Product ID with wildcard serial number
+      HidDevice trezor = hidServices.getHidDevice(0x534c, 0x01, null);
+      if (trezor != null) {
+        sendInitialise(trezor);
+      }
+
+    }
+
+  }
+
+  @Override
+  public void hidDeviceDetached(HidServicesEvent event) {
+
+    System.err.println("Device detached: " + event);
+
+
+
+  }
+
+  @Override
+  public void hidFailure(HidServicesEvent event) {
+
+    System.err.println("HID failure: " + event);
+
+  }
+
+  private void sendInitialise(HidDevice trezor) {
+
+    // Send the Initialise message
+    byte[] message = new byte[64];
+    for (int i = 0; i < 64; i++) {
+      message[i] = 0x00;
+    }
+    message[0] = 0x3f;
+    message[1] = 0x23;
+    message[2] = 0x23;
+
+    int val = trezor.write(message, PACKET_LENGTH, (byte) 0);
+    if (val != -1) {
+      System.out.println("> [" + val + "]");
+    } else {
+      System.err.println(trezor.getLastErrorMessage());
+    }
+
+    // Prepare to read a single data packet
+    boolean moreData = true;
+    while (moreData) {
+      byte data[] = new byte[PACKET_LENGTH];
+      // This method will now block for 500ms or until data is read
+      val = trezor.read(data, 500);
+      switch (val) {
+        case -1:
+          System.err.println(trezor.getLastErrorMessage());
+          break;
+        case 0:
+          moreData = false;
+          break;
+        default:
+          System.out.print("< [");
+          for (byte b : data) {
+            System.out.printf(" %02x", b);
+          }
+          System.out.println("]");
+          break;
+      }
+    }
+  }
+
+  /**
+   * Invokes {@code unit.}{@link java.util.concurrent.TimeUnit#sleep(long) sleep(sleepFor)}
+   * uninterruptibly.
+   */
+  public static void sleepUninterruptibly(long sleepFor, TimeUnit unit) {
+    boolean interrupted = false;
+    try {
+      long remainingNanos = unit.toNanos(sleepFor);
+      long end = System.nanoTime() + remainingNanos;
+      while (true) {
+        try {
+          // TimeUnit.sleep() treats negative timeouts just like zero.
+          NANOSECONDS.sleep(remainingNanos);
+          return;
+        } catch (InterruptedException e) {
+          interrupted = true;
+          remainingNanos = end - System.nanoTime();
+        }
+      }
+    } finally {
+      if (interrupted) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+}
