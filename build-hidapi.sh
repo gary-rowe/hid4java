3adcb3283096c5fbb3fc4eb71055f1254965b15d
Gary Rowe <g.rowe@froot.co.uk>
2019-11-08 16:48:04 +0000

    #83 First attempt at Android cross-compile (broken)

--- build-hidapi.sh
+++ build-hidapi.sh
@@ -35,6 +35,18 @@ docker run --rm dockcross/linux-armv6 > ./dockcross-linux-armv6
 chmod +x ./dockcross-linux-armv6
 mv ./dockcross-linux-armv6 /usr/local/bin
 
+# Android
+# 64 bit
+docker run --rm dockcross/android-arm64 > ./dockcross-android-arm64
+chmod +x ./dockcross-android-arm64
+mv ./dockcross-android-arm64 /usr/local/bin
+
+# 32 bit
+docker run --rm dockcross/android-arm > ./dockcross-android-arm
+chmod +x ./dockcross-android-arm
+mv ./dockcross-android-arm /usr/local/bin
+
+
 # Cross compilation
 cd ~/Workspaces/Cpp/hidapi/
 
@@ -72,6 +84,11 @@ cp linux/.libs/libhidapi-hidraw.so ../../Java/Personal/hid4java/src/main/resourc
 make
 cp mac/.libs/libhidapi.0.dylib ../../Java/Personal/hid4java/src/main/resources/darwin/libhidapi.dylib
 
+# Android environments (require libudev and libusb to be installed)
+# 64-bit
+dockcross-android-arm64 bash -c 'sudo make clean && sudo apt-get --yes install libudev-dev libusb-1.0-0-dev && sudo ./bootstrap && sudo ./configure --host=aarch64-linux-android && sudo make'
+cp linux/.libs/libhidapi-hidraw.so ../../Java/Personal/hid4java/src/main/resources/android-arm64/libhidapi.so
+
 # List all file info
 
 # Windows
@@ -82,9 +99,12 @@ file ../../Java/Personal/hid4java/src/main/resources/win32-x86/hidapi.dll
 # Linux
 file ../../Java/Personal/hid4java/src/main/resources/linux-x86-64/libhidapi.so
 file ../../Java/Personal/hid4java/src/main/resources/linux-amd64/libhidapi.so
-#file ../../Java/Personal/hid4java/src/main/resources/linux-arm/libhidapi.so
+file ../../Java/Personal/hid4java/src/main/resources/linux-arm/libhidapi.so
 
 # OS X
 file ../../Java/Personal/hid4java/src/main/resources/darwin/libhidapi.dylib
+
+# Android
+file ../../Java/Personal/hid4java/src/main/resources/android-arm64/libhidapi.so
 
 echo Done
